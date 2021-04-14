eb3c2dd5f4c92c6bf8fdb1d7f05d14ad2f61dd44
EleventhTen083-ASC <81180062+EleventhTen083-ASC@users.noreply.github.com>
2021-04-14 01:23:05 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+    apiKey: "AIzaSyA_CLlfSay6_opSCkaZre3MFglF7YAJfL8",
+    authDomain: "base42mjasc.firebaseapp.com",
+    projectId: "base42mjasc",
+    storageBucket: "base42mjasc.appspot.com",
+    messagingSenderId: "1053318452959",
+    appId: "1:1053318452959:web:4461417b8cf6a326593b6a",
+    measurementId: "G-1C7827N30F"
 });
